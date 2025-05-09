8165586e4f8ef181ce8fa6c7631c6a10c7f00741
mthrndr <kristian.d.nikolov@gmail.com>
2025-05-09 12:56:23 -0400

    lmao

--- hardware/nbody.sv
+++ hardware/nbody.sv
@@ -124,25 +124,6 @@ module nbody #(
                     //if go is not high, then we are not going to do anything (except take in writes from software)
                     // if we raised done, we are waiting for read to go high before dropping done
                     //once read and done are both low (and go is high obvisously), we can start the next cycle
-                    pos_input_1_addr <= addr[BODY_ADDR_WIDTH-1:0];
-
-
-                    //TODO: set addr things
-                    // All the memories get the addr.
-                    write_x_data <= writedata;
-                    write_y_data <= writedata;
-                    write_m_data <= writedata;
-                    write_vx <= writedata;
-                    write_vy_data <= writedata;
-
-                    // Write enable for the different memories
-                    wren_x <= (addr[15:9] == 7'b0000011) ? write : 0;
-                    wren_y <= (addr[15:9] == 7'b0000100) ? write : 0;
-                    wren_m <= (addr[15:9] == 7'b0000101) ? write : 0;
-                    wren_vx <= (addr[15:9] == 7'b0000110) ? write : 0;
-                    wren_vy <= (addr[15:9] == 7'b0000111) ? write : 0;
-
-
 
                     if(go == 1) begin //handshake logic
                         if(read_sw == 1) begin
