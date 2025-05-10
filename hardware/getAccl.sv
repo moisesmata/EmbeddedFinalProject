2a6cca544e5fedeb25d1bc3d12b0e87d647467d8
IsaacTrost <wisaactrost@gmail.com>
2025-05-10 13:50:22 -0400

    getAccl

--- hardware/getAccl.sv
+++ hardware/getAccl.sv
@@ -37,7 +37,7 @@ module getAccl #(
             y2_real <= y2;
             if (x1 == x2 && y1 == y2) begin
                 m2_real <= 0; // Set m2 to 0 if comparing a body to itself
-                x2_real <= ~x2; // Set x2 to a different value to avoid division by zero
+                x2_real = {x2[63], x2[62:52]+1, x2[51:0]+1};
             end else begin
                 m2_real <= m2;
                 x2_real <= x2;
