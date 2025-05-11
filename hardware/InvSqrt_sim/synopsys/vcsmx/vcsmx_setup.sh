17a46fdacd44306419ccbf00ae9246e93272e472
rlp2153 <rlp2153@micro24.ee.columbia.edu>
2025-05-11 19:17:06 -0400

    Added new Floating point ip cores

--- hardware/InvSqrt_sim/synopsys/vcsmx/vcsmx_setup.sh
+++ hardware/InvSqrt_sim/synopsys/vcsmx/vcsmx_setup.sh
@@ -12,7 +12,7 @@
 # or its authorized distributors. Please refer to the applicable 
 # agreement for further details.
 
-# ACDS 21.1 842 linux 2025.04.30.14:01:10
+# ACDS 21.1 842 linux 2025.05.11.17:44:10
 
 # ----------------------------------------
 # vcsmx - auto-generated simulation script
@@ -107,7 +107,7 @@
 # within the Quartus project, and generate a unified
 # script which supports all the Altera IP within the design.
 # ----------------------------------------
-# ACDS 21.1 842 linux 2025.04.30.14:01:10
+# ACDS 21.1 842 linux 2025.05.11.17:44:10
 # ----------------------------------------
 # initialize variables
 TOP_LEVEL_NAME="InvSqrt"
