3d0bf0ea61fc2f7cc1749dbd0d87e0994a8e77f6
robertpendergrast <rlp2153@columbia.edu>
2025-05-10 21:55:01 -0400

    Python Check Code Works

--- software/Tests/nbody.py
+++ software/Tests/nbody.py
@@ -4,22 +4,37 @@ import matplotlib.animation as animation
 from matplotlib.animation import FuncAnimation
 import csv
 
-def getA(R,M,N):
-    A = np.zeros((N,2))
-    F = np.zeros((N,2))
-
+def updateV(R,M,N,V,first = False):
     for i in range(N):
         for j in range(N):
-            if i != j: #needed to make sure we do not calculate infinite force between the same object
-                #print(np.linalg.norm((R[i]-R[j]))**3)
-                F[i] +=  1 * (M[j] * (R[i] - R[j]) / (np.linalg.norm((R[i]-R[j])))**3)
+            if i != j:
+                dx = R[i,0] - R[j,0]
+                dy = R[i,1] - R[j,1] 
 
-        A[i] = -1 * F[i]
+                dx2 = dx**2
+                dy2 = dy**2
 
-    return A
+                r2 = dx2 + dy2
+                r = (1/np.sqrt(r2))**3
 
+                if first:
+                    f = r*M[i]/2
+                else:
+                    f = r*M[i]
+
+                print(f"Intermediaries i: {i}, j: {j}, r2: {r2}, r: {r}, f: {f}, dvx: {dx*f}, dvy: {dy*f}")
+
+                vx = V[j,0] + dx*f
+
+                vy = V[j,1] + dy*f
+
+                V[j,0] =  vx
+                V[j,1] =  vy
+
+                print(V[j])
+    
 #N is the number of bodies, T is the total time, dt is the timestep
-def main(N,total_time, dt = 2, gap = 2):
+def main(N,total_time, dt = 1, gap = 1):
 
     gapstep = 0
     positions = np.zeros((int(total_time/dt)+1, N,2))
@@ -44,7 +59,8 @@ def main(N,total_time, dt = 2, gap = 2):
 
     #Get the initial acceleration:
     
-    A = getA(R,M,N)
+    updateV(R,M,N,V,True) #updates velocities
+    print(f"Velocities: \n{V}")
 
     fig, ax = plt.subplots()
     line, = ax.plot([], [], 'bo')  # blue line with circle markers
@@ -61,15 +77,11 @@ def main(N,total_time, dt = 2, gap = 2):
 
         line.set_data(positions[gapstep,:,0], positions[gapstep,:,1])
         plt.pause(0.01)
+ 
+        R += V * dt #update positions
+        updateV(R,M,N,V,False) #update velocities
 
-        if first:
-            V += A * dt/2 # Calculate v i+1/2
-            first = False
-        else:
-            V += A * dt
-
-        R += V * dt # Calculate x i+1
-        A = getA(R,M,N) # Calculate a i+1
+        print(f"Velocities: \n{V}")
 
         t += dt
         if gapstep % gap == 0:
