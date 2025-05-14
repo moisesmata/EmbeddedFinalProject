92fa3e33a70017cc2c71d63fc003daa50cc5fb14
Moises Mata <mm6155@columbia.edu>
2025-05-14 05:51:32 -0400

    mass scaling

--- software/nbody_display/display.c
+++ software/nbody_display/display.c
@@ -14,7 +14,7 @@
 #define MAXCHAR 100000
 #define CSV_FILENAME "nbody_results.csv"
 #define PLAYBACK_DELAY_MS 100  
-#define MAX_TIMESTEPS 1000   //Fixed allocation (will fix later)
+#define MAX_TIMESTEPS 10000   //Fixed allocation (will fix later)
 
 // Function to convert nbody simulation coordinates to display coordinates
 static void convert_coordinates(float nbody_x, float nbody_y, 
