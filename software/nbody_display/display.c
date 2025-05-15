7e40891814656ab1c19d6845101d004322e72cc0
IsaacTrost <wisaactrost@gmail.com>
2025-05-14 21:12:27 -0400

    changed software added dt

--- software/nbody_display/display.c
+++ software/nbody_display/display.c
@@ -64,7 +64,7 @@ static int parse_csv_line(char* line, body_full* arg, int max_bodies) {
         arg[i].x = x;
         arg[i].y = y;
         arg[i].n = i;
-        arg[i].m = m;
+        arg[i].m = cbrtf(m);
         num_bodies++;
     }
     
